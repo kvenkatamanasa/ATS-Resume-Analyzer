6bb15f8b4fef55d409ab53ebef9b6fed78043783
Kammineni Venkata Manasa <kvenkatamanasa@gmail.com>
2026-08-30 00:34:47 +0530

    Update wsgi.py

--- ats_analyzer/wsgi.py
+++ ats_analyzer/wsgi.py
@@ -4,7 +4,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    "ats_resume_analyzer.settings"
+    "ats_analyzer.settings"
 )
 
 application = get_wsgi_application()
